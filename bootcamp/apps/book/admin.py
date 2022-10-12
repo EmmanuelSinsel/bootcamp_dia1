b5b8ea3031a67f407fc364308def1c8a7f10e853
EmmanuelSinsel <95643610+EmmanuelSinsel@users.noreply.github.com>
2022-10-12 10:22:48 -0600

    Update admin.py

--- bootcamp/apps/book/admin.py
+++ bootcamp/apps/book/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from apps.book.models import Category, Book
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Category)
+admin.site.register(Book)#SE REGISTRA LA CLASE LIBRO
+admin.site.register(Category)#SE REGISTRA LA CLASE CATEGORIA
