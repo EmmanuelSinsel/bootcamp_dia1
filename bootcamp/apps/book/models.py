693db8f112a8ebd153f13218ba7dc3277e9d3e9d
EmmanuelSinsel <95643610+EmmanuelSinsel@users.noreply.github.com>
2022-10-12 10:18:49 -0600

    Update models.py

--- bootcamp/apps/book/models.py
+++ bootcamp/apps/book/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+#MODELO DE CATEGORIA
 class Category(models.Model):
     name = models.CharField(verbose_name='Nombre', max_length=50)
 
@@ -11,12 +11,12 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
+#MODELO DE LIBRO
 class Book(models.Model):
     title = models.CharField(max_length=150, verbose_name='Titulo')
     publication_date = models.DateField(verbose_name='Fecha de Publicacion')
     pages = models.PositiveSmallIntegerField(verbose_name="Numero de Paginas")
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null = True, verbose_name='Categoria')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null = True, verbose_name='Categoria') #LLAVE FORANEA DE CATEGORIA EN LIBRO
 
 
     class Meta:
