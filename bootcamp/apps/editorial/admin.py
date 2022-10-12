66131b30ef90d40b6026aed541662e4cabac3bbf
EmmanuelSinsel <95643610+EmmanuelSinsel@users.noreply.github.com>
2022-10-12 10:21:52 -0600

    Update admin.py

--- bootcamp/apps/editorial/admin.py
+++ bootcamp/apps/editorial/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
 from apps.editorial.models import Editorial
-# Register your models here.
-admin.site.register(Editorial)
+
+admin.site.register(Editorial)#SE REGISTRA LA CLASE EDITORIAL
