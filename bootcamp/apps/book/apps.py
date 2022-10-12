1ea12c9b6701a827b016a5be33c09c39fff3993e
EmmanuelSinsel <95643610+EmmanuelSinsel@users.noreply.github.com>
2022-10-12 10:24:10 -0600

    Update apps.py

--- bootcamp/apps/book/apps.py
+++ bootcamp/apps/book/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BookConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.book'
+    name = 'apps.book' #SE AÑADE APPS. PARA INDICAR LA CARPETA CONTENEDORA
