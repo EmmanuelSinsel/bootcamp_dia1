51c96e5a06a0cd7a8e1ccdf8f2a19ed488296fd3
EmmanuelSinsel <95643610+EmmanuelSinsel@users.noreply.github.com>
2022-10-12 10:22:18 -0600

    Update admin.py

--- bootcamp/apps/author/admin.py
+++ bootcamp/apps/author/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
 from apps.author.models import Author
-# Register your models here.
-admin.site.register(Author)
+
+admin.site.register(Author)#SE REGISTRA LA CLASE AUTOR
