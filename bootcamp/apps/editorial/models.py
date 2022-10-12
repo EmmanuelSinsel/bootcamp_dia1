56539b041ce1f1e24cfa9e33612df3d1df21d533
EmmanuelSinsel <95643610+EmmanuelSinsel@users.noreply.github.com>
2022-10-12 10:19:56 -0600

    Update models.py

--- bootcamp/apps/editorial/models.py
+++ bootcamp/apps/editorial/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-
 from apps.user.models import User
 
-
+#MODELO DE EDITORIAL
 class Editorial(models.Model):
     name = models.CharField(max_length=100, verbose_name='Nombre')
     address = models.CharField(max_length=200, verbose_name='Direccion')
